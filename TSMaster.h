7468969dbf61843a2a40d6a0a4461248b1643a80
YulyOta1 <154386909+YulyOta1@users.noreply.github.com>
2025-05-12 10:54:36 +0300

    Update TSMaster.h

--- TSMaster.h
+++ TSMaster.h
@@ -1,37 +1,56 @@
 #ifndef _TSMaster_H
 #define _TSMaster_H
 
-#include <cstring>  // Для std::memcpy
+#include <cstring>
 #include <math.h>
 #include <stdio.h>
 
 #ifndef __cplusplus
 #include <stdbool.h>
 #endif
+
+// ===================== КОНФИГУРАЦИЯ МАКРОСОВ =====================
 #ifdef TSMASTER_NO_AUTO_PROPERTIES
-// GCC-совместимые макросы
-#define PROPERTY(type, name) \
-    private: \
-        type m_##name; \
-    public: \
-        type get_##name() const { return m_##name; } \
-        void set_##name(const type& value) { m_##name = value; }
+    // Режим GCC - свойства через getter/setter
+    #define PROPERTY(type, name) \
+        private: \
+            type m_##name; \
+        public: \
+            type get_##name() const { return m_##name; } \
+            void set_##name(const type& value) { m_##name = value; }
 
-#define ARRAY_PROPERTY(type, name, size) \
-    private: \
-        type m_##name[size]; \
-    public: \
-        type get_##name(int index) const { return m_##name[index]; } \
-        void set_##name(int index, const type& value) { m_##name[index] = value; }
+    #define ARRAY_PROPERTY(type, name, size) \
+        private: \
+            type m_##name[size]; \
+        public: \
+            type get_##name(int index) const { return m_##name[index]; } \
+            void set_##name(int index, const type& value) { m_##name[index] = value; }
 
+    // Удаляем MSVC+ SPECIFIC MACROS
+    #undef READONLY_PROPERTY
+    #undef WRITEONLY_PROPERTY
+    #undef GET
+    #undef SET
+    #undef ARRAY_GET
+    #undef ARRAY_SET
 #else
-// Оригинальные MSVC-макросы
-#define PROPERTY(t, n)  __declspec(property(put = property__set_##n, get = property__get_##n)) t n;\
-    typedef t property__tmp_type_##n
-#define READONLY_PROPERTY(t, n) __declspec(property(get = property__get_##n)) t n;\
-    typedef t property__tmp_type_##n
-#define ARRAY_PROPERTY(t, n, s) __declspec(property(put = property__set_##n, get = property__get_##n)) t n[s];\
-    typedef t property__tmp_type_##n
+    // Режим MSVC - оригинальные макросы
+    #define PROPERTY(t,n)  __declspec(property(put=property__set_##n, get=property__get_##n)) t n;\
+        typedef t property__tmp_type_##n
+    
+    #define READONLY_PROPERTY(t,n) __declspec(property(get=property__get_##n)) t n;\
+        typedef t property__tmp_type_##n
+    
+    #define WRITEONLY_PROPERTY(t,n) __declspec(property(put=property__set_##n)) t n;\
+        typedef t property__tmp_type_##n
+    
+    #define ARRAY_PROPERTY(t,n,s) __declspec(property(put=property__set_##n, get=property__get_##n)) t n[s];\
+        typedef t property__tmp_type_##n
+    
+    #define GET(n) property__tmp_type_##n property__get_##n()
+    #define SET(n) void property__set_##n(const property__tmp_type_##n& value)
+    #define ARRAY_GET(n) property__tmp_type_##n property__get_##n(int index)
+    #define ARRAY_SET(n) void property__set_##n(int index, const property__tmp_type_##n& value)
 #endif
 
 #define CH1 0
