46909bf4f0d2c6da60203efd7016e273a998d779
YulyOta1 <154386909+YulyOta1@users.noreply.github.com>
2025-05-13 09:21:39 +0300

    Update TSMaster_modified.h

--- TSMaster_modified.h
+++ TSMaster_modified.h
@@ -1,13 +1,31 @@
-// Disable problematic Windows attributes for Linux
+// TSMaster_modified.h
+#ifndef TSMaster_MODIFIED_H
+#define TSMaster_MODIFIED_H
+
+// Disable Windows-specific attributes completely
+#undef __stdcall
 #define __stdcall
+
+#undef __declspec
 #define __declspec(x)
 
-// Completely disable the property system
-#define PROPERTY(type, name) type m_##name;
-#define ARRAY_PROPERTY(type, name, size) type m_##name[size];
+// Completely replace the property system with simple member variables
+#undef PROPERTY
+#define PROPERTY(type, name) \
+    private: type m_##name; \
+    public: type get_##name() const { return m_##name; } \
+    void set_##name(const type& value) { m_##name = value; }
 
-// Include the original header with guards against multiple inclusion
+#undef ARRAY_PROPERTY
+#define ARRAY_PROPERTY(type, name, size) \
+    private: type m_##name[size]; \
+    public: type get_##name(int index) const { return m_##name[index]; } \
+    void set_##name(int index, const type& value) { m_##name[index] = value; }
+
+// Include the original header with protection against multiple inclusion
 #ifndef ORIGINAL_TSMASTER_INCLUDED
 #define ORIGINAL_TSMASTER_INCLUDED
 #include "TSMaster.h"
 #endif
+
+#endif // TSMaster_MODIFIED_H
